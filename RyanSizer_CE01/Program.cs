0a6bd895e1456bc725fbe9ba8bbdc82c14c68995
Ryan Sizer <rlsizer@fullsail.edu>
2017-06-04 10:06:51 -0400

    Set up arrays.

--- RyanSizer_CE01/Program.cs
+++ RyanSizer_CE01/Program.cs
@@ -10,6 +10,25 @@ namespace RyanSizer_CE01
     {
         static void Main(string[] args)
         {
+            /*
+            Ryan Sizer
+            Section 01
+            Advanced Scalable Data Infrastructures
+            06/03/2017
+            Big Blue Fish
+            */
+
+
+            //Declare and define an array for the fish colors
+            string[] fishColors = new string[12] { "red", "blue", "green", "yellow", "red", "blue", "blue", "green", "yellow", "red", "blue", "green" };
+
+            //Declare and define an array for the fish lengths
+            float[] fishLength = new float[12] {3.755F, 8.881F, 6.5F, 1.95F, 3.001F, 17.233F, 9.991F, 6.777F, 5.15F, 4.9F, 4.25F, 8.005F };
+
+
+
+
+
         }
     }
 }
